c713eca789d930d67f863017684d510419643b62
LucidMortem <56680954+LucidMortem@users.noreply.github.com>
2020-07-03 02:18:26 +0300

    Okunabilir yapma
    virgüller arasına boşluk koyarak global geleneklere uygun bir hale getirip daha okunabilir hale soktum.

--- soru3.py
+++ soru3.py
@@ -1,14 +1,15 @@
-"""Modül:"""
+"""Modül'ün kendisi:"""
 def giren_cikan(ayrilanlar_listesi, personel_listesi):
     toplam_ayrilan = sum(ayrilanlar_listesi)
     ortalama_personel = sum(personel_listesi) / len(personel_listesi)
     devir_hizi = toplam_ayrilan / ortalama_personel * 100
     return toplam_ayrilan, ortalama_personel, devir_hizi
 
+
 """Modülün çalışma örneği:"""
-ayrilanlar = [5,10,15,13]
-personeller= [100,95,110,120]
-ayrilan, ortalama, devir = giren_cikan(ayrilanlar,personeller)
+ayrilanlar = [5, 10, 15, 13]
+personeller = [100, 95, 110, 120]
+ayrilan, ortalama, devir = giren_cikan(ayrilanlar, personeller)
 print(ayrilan)
 print(ortalama)
-print((devir))
+print(devir)
