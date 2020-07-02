2c2070f97b82da9fd6dbba0101cdf3e5e54ffc86
LucidMortem <56680954+LucidMortem@users.noreply.github.com>
2020-07-03 02:22:03 +0300

    Update soru4.py

--- soru4.py
+++ soru4.py
@@ -13,6 +13,7 @@ plt.plot(miktar, sabit_gider, label="Sabit Gider")
 plt.plot(miktar, satis_gelirleri, label="Satış Gelirleri")
 plt.plot(miktar, degisken_giderler, label="Degisken Giderler")
 plt.plot(miktar, toplam_giderler, label="Toplam  Gider")
+
 plt.ylabel("Gelir-Gider")
 plt.xlabel("Miktar")
 plt.title("Başa Baş Grafiği")
